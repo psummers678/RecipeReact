ab37e734dedebb2d134b39880e9742474ab77568
phillip <phillipgsummers@gmail.com>
2022-03-23 18:01:36 +0000

    Added Layout to singleRecipe View

--- src/components/RecipeComponents/RecipeDetailedView.tsx
+++ src/components/RecipeComponents/RecipeDetailedView.tsx
@@ -34,38 +34,33 @@ class RecipeDetailedView extends React.Component<{ recipeId: number }, { recipe:
 
     render(): React.ReactNode {
         let recipe = this.state.recipe;
-        return (
-            <>
-                <h1>{recipe.recipeName}</h1>
-                <h3>{recipe.recipeDurationMins}</h3>
-                <h3>{recipe.rating}</h3>
-                <h3>{recipe.difficulty}</h3>
-                <p>{recipe.recipeBody}</p>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Ingredients</th>
-                            <th>Amount</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { Object.entries(recipe.ingredientRequirements).map( ([ingredient, amount]) => {
-                            // ingredient: "1,Steak" 
-                            // amount: "5tsp"#
-                            console.log("SPLIT");
-                            let [ingredientId, ingredientName] = ingredient.replaceAll('"','').split(",");
-                            return (
-                                <tr key={ingredientId}>
-                                    <td>{ingredientName}</td>
-                                    <td>{amount}</td>
-                                </tr>
-                            )
-                        })}
-                
-                    </tbody>
-                </table>
-                
-            </>
+        return (<>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-8">
+                        <h2>{recipe.recipeName}</h2>
+                        <ul>
+                            <li>Duration: {recipe.recipeDurationMins}</li>
+                            <li>Rating: {recipe.rating}</li>
+                            <li>Difficulty: {recipe.difficulty}</li>
+                        </ul>
+                    </div>
+                    <div className="col-4">
+                        <ul>Ingredients
+                            {Object.entries(recipe.ingredientRequirements).map(([ingredient, amount]) => {
+                                    let [ingredientId, ingredientName] = ingredient.replaceAll('"', '').split(",");
+                                    return (
+                                        <li key={ingredientId}>{amount} of {ingredientName}</li>
+                                    )})}
+                        </ul>
+                    </div>
+                </div>
+                <div className="row">
+                    <p>{recipe.recipeBody}</p>
+
+                </div>
+            </div>
+        </>
         )
     }
 }
