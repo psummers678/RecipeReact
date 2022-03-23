25cf5e0091a64cae710f083e87192f6418a0dae5
phillip <phillipgsummers@gmail.com>
2022-03-23 17:45:38 +0000

    Added layout to RecipeTable component

--- src/components/RecipeComponents/RecipeListContainer.tsx
+++ src/components/RecipeComponents/RecipeListContainer.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import RecipeTable from './RecipeTable';
 
-class RecipeListView extends React.Component<{},{}>{
+class RecipeListView extends React.Component<{}, {}>{
 
 
     render(): React.ReactNode {
-        return(
-            <div className="list-view-container"> 
-                <h1> All Recipes</h1>
-                <div >
-                    <RecipeTable />
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <h1> All Recipes</h1>
+                </div>
+                <div className="row">
+                    <div className="col-md-8 col">
+                        <RecipeTable />
+                    </div>
+                    <div className="col-sm">
+                        <ul>
+                            <li>Listed Ingredients</li>
+                            <li>Filters?</li>
+                            <li>Examples?</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
