ee806a252f45d70fede11a925668b36e68f37afa
phillip <phillipgsummers@gmail.com>
2022-03-23 17:50:39 +0000

    Added SingleRecipeOverview layout

--- src/components/RecipeComponents/RecipeSingleView.tsx
+++ src/components/RecipeComponents/RecipeSingleView.tsx
@@ -5,10 +5,24 @@ import RecipeDetailedView from "./RecipeDetailedView";
 function RecipeSingleView() {
     return (
         <>
-            <h1>THIS IS A SINGLE RECIPE VIEW</h1>
-            <h2>The id passed is {useParams().id} </h2>
-            <RecipeDetailedView recipeId={Number(useParams().id)} />
-            <div><p>This will eventually be a sidebar with a list of all recipes for easy switching</p></div>
+            <div className="row">
+                <div className="col-md-8">
+                    <div className="row">
+                        <h1>THIS IS A SINGLE RECIPE VIEW</h1>
+                        <h2>The id passed is {useParams().id} </h2>
+                    </div>
+                    <div className="row">
+                        <RecipeDetailedView recipeId={Number(useParams().id)} />
+                    </div>
+                </div>
+                <div className="col sidebar">
+                    <ul>This will eventually be a sidebar with a list of all recipes for easy switching like
+                        <li>Mac</li>
+                        <li>And</li>
+                        <li>Cheese</li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
